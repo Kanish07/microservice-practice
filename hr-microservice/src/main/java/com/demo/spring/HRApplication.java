965e77f75b54ec47573e58ebab9bd3164eb06080
Kanishkar Bubeshkumar <kanishkar@Kanishkars-MacBook-Air.local>
2021-11-10 23:16:43 +0530

    Initial Microservice commit

---
+++ hr-microservice/src/main/java/com/demo/spring/HRApplication.java
@@ -0,0 +1,24 @@
+package com.demo.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
+public class HRApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HRApplication.class,args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
---
+++ hr-microservice/src/main/java/com/demo/spring/HRController.java
@@ -0,0 +1,26 @@
+package com.demo.spring;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class HRController {
+    @Autowired
+    RestTemplate rt;
+
+    @GetMapping(path="/hr/get")
+    @HystrixCommand(fallbackMethod = "fallbackGetEmp")
+    public ResponseEntity<String> getEmpDetails(@RequestParam("id") int id){
+        return rt.getForEntity("http://emp-service/emp/find/"+id,String.class);
+    }
+
+    public ResponseEntity<String> fallbackGetEmp(int id){
+        return ResponseEntity.ok("Server Down...");
+    }
+}
